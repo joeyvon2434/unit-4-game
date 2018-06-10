95a2ee5dee80c6898b0e8023092d7aeb7363e1ac
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-10 16:50:06 -0400

    starting battle javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,38 +2,47 @@
 
 
 //Variable lists
-   var selHP =1;
-   var selAttack =1;
-   var selOriginalAttack =1;
-   var selectedFighter ='';
-   var characterSourceImage ='';
+var selHP = 1;
+var selAttack = 1;
+var selOriginalAttack = 1;
+var selectedFighter = '';
+var characterSourceImage = '';
+var currentOpponent = '';
+var defeatedOpponentCounter = 0;
+var opponentHP = 0;
+var opponentCounterAttack = 0;
 
 
-   $( document ).ready(function () {
+$(document).ready(function () {
 
-
-//Click funtion to define selected fighter and write in initial stats from data attributes in HTML
-//and continue to set up the character and remaining opponents boxes
+//BEGIN MAJOR SECTION 1: Game Set Up
+//============================================
+    //Click funtion to define selected fighter and write in initial stats from data attributes in HTML
+    //and continue to set up the character and remaining opponents boxes
+    
     $(".star-ship").on("click", function () {
         selectedFighter = $(this).data("ship");
-            selHP = $(this).data("health");
-            selAttack = $(this).data("attack");
-            selOriginalAttack = $(this).data("counter-attack");
-            //**** if statement to set picture source to correct fighter in the character box, or 
-            //use another data attribute to define the source
-            characterSourceImage = $(this).data('image-link');
-            $('#chosen-character').attr('src',characterSourceImage);
-            // set display of the character select box to 'none'
+        selHP = $(this).data("health");
+        selAttack = $(this).data("attack");
+        selOriginalAttack = $(this).data("counter-attack");
+
+        //**** if statement to set picture source to correct fighter in the character box, or 
+        //use another data attribute to define the source
+
+        var characterSourceImage = $(this).data('image-link');
+        $('#chosen-character').attr('src', characterSourceImage);
+
+        // set display of the character select box to 'none'
+
         $('#chooseCharacter').hide();
-        console.log(selectedFighter);
-        console.log(selHP);
-        console.log(selAttack);
-        console.log(selOriginalAttack);
+
         //set game box to display, and display the non-selected characters in the remaining window
-            //game Box
+        //game Box
+
         $('#game-box').show();
 
-            //Hide selected fighter from remaining fighters box
+        //Hide selected fighter from remaining fighters box
+
         if ($('#xWing').data('ship') == selectedFighter) {
             $('#xWing').hide();
         };
@@ -46,10 +55,62 @@
         if ($('#tieInterceptor').data('ship') == selectedFighter) {
             $('#tieInterceptor').hide();
         };
+
+        //Hide images in defeated opponents box
+
+        $('.defeated-opponents').hide();
+
+        alert('Choose your first opponent on the right!');
     });
+//END MAJOR SECTION ONE: Game Set Up
+//=========================================================
+//BEGIN MAJOR SECTION TWO: Opponent Selection
 
-//Section to display opponents after the player selects their character
+    //Possibly add boolean to ensure a new opponent can only be chosen
+    //after the current opponent is defeated
+
+    //Identify selected opponent and display image in current opponent box and populate current opponent stats
+
+    $('.up-next').on('click', function() {
+        currentOpponent = $(this).data('ship');
+        var opponentImage = $(this).data('image-link');
+        opponentHP = $(this).data('health');
+        opponentCounterAttack = $(this).data('counter-attack');
+        $('#current-opponent').attr('src',opponentImage);
+        
+    //Hide selected opponent in remaining opponents box
+
+    $(this).hide();
+
+    });//Close Opponent Selection
+
+
+//END MAJOR SECTION TWO: Opponent Selection
+//=================================================
+//BEGIN MAJOR SECTION THREE: Attack Dynamics and Battle
+
+    //Use Fire! button to trigger attack, decrease opponent health, and increase attack power
+    
+
+    //Use if statement to see if opponent is defeated. Move current opponent to defeated box add 1 to defeated 
+    //opponent counter and check for victorty. If victorious, display a you win screen with
+    //a new game button.
+        $('#fire-button').on('click', function() {
 
 
 
+    //Continue with opponents counter attck and health decrease of character
+
+
+    //Use if statement to see if character is defeated, if so, display game over and New Game button
+
+
+
+//END MAJOR SECTION THREE: Attack Dynamics and Battle
+//=================================================
+//BEGIN MAJOR SECTION FOUR: Moving defeated characters s
+
+
+
+    });
 });
