c9492f8b8bae9f485b13140b54356a2977103a90
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-10 13:43:45 -0400

    format css for filling the remining box

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,25 @@
 //Variable lists
    var selHP =1;
    var selAttack =1;
-   var originalAttack =1;
+   var selOriginalAttack =1;
 
 
    $( document ).ready(function () {
 
 
-//Click funtion to define selected fighter and write in initial stats
+//Click funtion to define selected fighter and write in initial stats rom data attributes in HTML
     $(".star-ship").on("click", function () {
-        selectedFighter = $(this).attr('value');
+        selectedFighter = $(this).data("ship");
             selHP = $(this).data("health");
             selAttack = $(this).data("attack");
             selOriginalAttack = $(this).data("counter-attack");
-            //if statement to set picture source to correct fighter, or us another data attribute
-        
-
+            //**** if statement to set picture source to correct fighter in the character box, or us another data attribute
+            //**** set display of the character select box to 'none'
+            //**** set display of game box to show
+        console.log(selectedFighter);
+        console.log(selHP);
+        console.log(selAttack);
+        console.log(selOriginalAttack);
     });
 
 
