b9996dca2f36ca6ae036979d9b3c58d33b6073be
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-09 22:20:12 -0400

    add variables for each fighter to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//Javascript file containing working code for the Star Wars: Starfighter Challenge
+
+var xWing = {
+    HP: 200,
+    attackPower: 15,
+    counterAttack: 10,
+}
+
+var aWing = {
+    HP: 120,
+    attackPower: 25,
+    counterAttack: 15,
+}
+
+var tieFighter = {
+    HP: 150,
+    attackPower: 5,
+    counterAttack: 25,
+}
+
+var tieInterceptor = {
+    HP: 175,
+    attackPower: 20,
+    counterAttack: 20,
+}
+
